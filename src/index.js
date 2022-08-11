88b23f2cb2420375cf4d10bedd188a36df4189b8
zzuhann <zzuhanlin@gmail.com>
2022-08-11 15:38:54 +0800

    update styled component to Link and firebase

--- src/index.js
+++ src/index.js
@@ -8,9 +8,30 @@ import ThankYou from "./pages/ThankYou/ThankYou";
 import Product from "./pages/Product/Product";
 import Profile from "./pages/Profile/Profile";
 import SecondHandClothing from "./pages/SecondHandClothing/SecondHandClothing";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+// Import the functions you need from the SDKs you need
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAkIcBB1qLOgkNNZkuSdV1-ZyoSL-ajGGk",
+  authDomain: "stylish-pro.firebaseapp.com",
+  projectId: "stylish-pro",
+  storageBucket: "stylish-pro.appspot.com",
+  messagingSenderId: "39530697970",
+  appId: "1:39530697970:web:2e8729b4550d323686ec6b",
+  measurementId: "G-YJ5C91D5C2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 root.render(
   <BrowserRouter>
     <Routes>
